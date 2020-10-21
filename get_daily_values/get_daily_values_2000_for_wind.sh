86964b53dd5a7bf6f198da36c771755e9078e4f5
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-10-21 15:07:54 +0200

    get daily values for wind

--- get_daily_values/get_daily_values_2000_for_wind.sh
+++ get_daily_values/get_daily_values_2000_for_wind.sh
@@ -1,59 +1,73 @@
 
 set -x
 
-MAIN_HOURLY_SOURCE_DIR="/scratch-shared/edwinhs/test_arise_meteo/hourly/"
+YEAR=2017
+YEAR_PLUS_1=$(( YEAR + 1 ))
 
-#~ edwinhs@fcn17.bullx:/scratch-shared/edwinhs/test_arise_meteo/hourly$ ls -lah
-#~ total 64K
-#~ drwxr-xr-x 8 edwinhs edwinhs 4.0K Mar 11 16:18 .
-#~ drwxr-xr-x 3 edwinhs edwinhs 4.0K Mar 12 00:40 ..
-#~ drwxr-xr-x 2 edwinhs edwinhs 4.0K Mar 11 16:54 Variable_Falb
-#~ drwxr-xr-x 2 edwinhs edwinhs 4.0K Mar 11 16:54 Variable_Snsr
-#~ drwxr-xr-x 2 edwinhs edwinhs 4.0K Mar 11 16:55 Variable_Spre
-#~ drwxr-xr-x 2 edwinhs edwinhs 4.0K Mar 11 17:37 Variable_Temp
-#~ drwxr-xr-x 2 edwinhs edwinhs 4.0K Mar 11 16:55 Variable_Tpre
-#~ drwxr-xr-x 2 edwinhs edwinhs 4.0K Mar 11 17:30 Variable_Wind
+OUTPUT_FOLDER="/scratch/depfg/sutan101/test_arise_meteo_europe/daily_before_remapcon/"
+mkdir -p ${OUTPUT_FOLDER}
+cd ${OUTPUT_FOLDER}
 
-DAILY_OUTPUT_FOLDER="/scratch-shared/edwinhs/test_arise_meteo/daily/"
-mkdir -p ${DAILY_OUTPUT_FOLDER}
+MAIN_HOURLY_SOURCE_DIR="/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly/"
+
+#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$ ls -lah
+#~ total 4.0K
+#~ drwxr-xr-x 8 hoch0001 depfg   6 Oct 14 22:30 .
+#~ drwxr-xr-x 3 hoch0001 depfg   1 Oct 14 11:14 ..
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:10 Variable_Falb
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:03 Variable_Snsr
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:09 Variable_Spre
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 19 03:09 Variable_Temp
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:12 Variable_Tpre
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 19 03:11 Variable_Wind
 
 # tp, total precipitation
 HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Tpre
 # - daily total precipitation, NOTE: using daymax (as hourly source data are accumulative on each day)
-cdo -L -b F64 -settime,00:00:00 -setunit,m.day-1 -daymax -selyear,2000/2000 -shifttime,-25min -selvar,tp -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_total-preci_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_total-preci_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,m.day-1      -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,tp  -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
 # ssr, surface solar radiation
 HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Snsr
 # - daily total, NOTE: using daymax (as hourly source data are accumulative on each day)
-cdo -L -b F64 -settime,00:00:00 -setunit,J.m-2.day-1 -daymax -selyear,2000/2000 -shifttime,-25min -selvar,ssr -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_total-ssrad_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_total-ssrad_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,J.m-2.day-1  -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,ssr -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
 # d2m
 HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Temp
 # - maximum
-cdo -L -b F64 -settime,00:00:00 -setunit,K -daymax -selyear,2000/2000 -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_d2m-maximum_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-maximum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 # - mean
-cdo -L -b F64 -settime,00:00:00 -setunit,K -daymean -selyear,2000/2000 -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_d2m-average_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-average_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 # - minimum
-cdo -L -b F64 -settime,00:00:00 -setunit,K -daymin -selyear,2000/2000 -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_d2m-minimum_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-minimum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymin  -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
 # t2m
 HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Temp
 # - maximum
-cdo -L -b F64 -settime,00:00:00 -setunit,K -daymax -selyear,2000/2000 -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_t2m-maximum_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-maximum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 # - mean
-cdo -L -b F64 -settime,00:00:00 -setunit,K -daymean -selyear,2000/2000 -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_t2m-average_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-average_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 # - minimum
-cdo -L -b F64 -settime,00:00:00 -setunit,K -daymin -selyear,2000/2000 -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_t2m-minimum_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-minimum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymin  -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
 # fal
 HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Falb
 # - mean
-cdo -L -b F64 -settime,00:00:00 -setunit,dimensionless -daymean -selyear,2000/2000 -shifttime,-25min -selvar,fal -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_fal-average_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_fal-average_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,dimensionless -daymean -selyear,${YEAR} -shifttime,-25min -selvar,fal -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
 # sp
 HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Spre
 # - mean
-cdo -L -b F64 -settime,00:00:00 -setunit,Pa -daymean -selyear,2000/2000 -shifttime,-25min -selvar,sp -mergetime ${HOURLY_SOURCE_DIR}/*.nc ${DAILY_OUTPUT_FOLDER}/tanzania_era5-land_daily_spressu-avg_2000-2000.nc &
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_spressu-avg_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,Pa            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,sp  -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
 wait
 
