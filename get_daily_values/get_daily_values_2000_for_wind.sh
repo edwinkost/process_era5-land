73e3f20aa4b7e81cf67f211700faa4790e5c7225
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-10-21 15:08:41 +0200

    get daily values for wind

--- get_daily_values/get_daily_values_2000_for_wind.sh
+++ get_daily_values/get_daily_values_2000_for_wind.sh
@@ -10,64 +10,71 @@ cd ${OUTPUT_FOLDER}
 
 MAIN_HOURLY_SOURCE_DIR="/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly/"
 
-#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$ ls -lah
-#~ total 4.0K
-#~ drwxr-xr-x 8 hoch0001 depfg   6 Oct 14 22:30 .
-#~ drwxr-xr-x 3 hoch0001 depfg   1 Oct 14 11:14 ..
-#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:10 Variable_Falb
-#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:03 Variable_Snsr
-#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:09 Variable_Spre
-#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 19 03:09 Variable_Temp
-#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:12 Variable_Tpre
-#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 19 03:11 Variable_Wind
+#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$
+#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$ ls -lah Variable_Wind/*2000*
+#~ -rw-r--r-- 1 hoch0001 depfg 2.4G Oct 16 19:33 Variable_Wind/ERA5-Land_10u10v_200001.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.3G Oct 16 19:48 Variable_Wind/ERA5-Land_10u10v_200002.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.4G Oct 16 20:02 Variable_Wind/ERA5-Land_10u10v_200003.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.3G Oct 16 20:17 Variable_Wind/ERA5-Land_10u10v_200004.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.4G Oct 16 20:31 Variable_Wind/ERA5-Land_10u10v_200005.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.3G Oct 16 20:46 Variable_Wind/ERA5-Land_10u10v_200006.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.4G Oct 16 21:01 Variable_Wind/ERA5-Land_10u10v_200007.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.4G Oct 16 21:15 Variable_Wind/ERA5-Land_10u10v_200008.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.3G Oct 16 21:29 Variable_Wind/ERA5-Land_10u10v_200009.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.4G Oct 16 21:43 Variable_Wind/ERA5-Land_10u10v_200010.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.3G Oct 16 21:57 Variable_Wind/ERA5-Land_10u10v_200011.nc
+#~ -rw-r--r-- 1 hoch0001 depfg 2.4G Oct 16 22:11 Variable_Wind/ERA5-Land_10u10v_200012.nc
+#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$
+#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$ ncdump -h Variable_Wind/ERA5-Land_10u10v_200001.nc
+#~ netcdf ERA5-Land_10u10v_200001 {
+#~ dimensions:
+        #~ longitude = 1061 ;
+        #~ latitude = 801 ;
+        #~ time = 744 ;
+#~ variables:
+        #~ float longitude(longitude) ;
+                #~ longitude:units = "degrees_east" ;
+                #~ longitude:long_name = "longitude" ;
+        #~ float latitude(latitude) ;
+                #~ latitude:units = "degrees_north" ;
+                #~ latitude:long_name = "latitude" ;
+        #~ int time(time) ;
+                #~ time:units = "hours since 1900-01-01 00:00:00.0" ;
+                #~ time:long_name = "time" ;
+                #~ time:calendar = "gregorian" ;
+        #~ short u10(time, latitude, longitude) ;
+                #~ u10:scale_factor = 0.000665902840398635 ;
+                #~ u10:add_offset = 5.93637359154855 ;
+                #~ u10:_FillValue = -32767s ;
+                #~ u10:missing_value = -32767s ;
+                #~ u10:units = "m s**-1" ;
+                #~ u10:long_name = "10 metre U wind component" ;
+        #~ short v10(time, latitude, longitude) ;
+                #~ v10:scale_factor = 0.000712566187422019 ;
+                #~ v10:add_offset = 0.00739804277298821 ;
+                #~ v10:_FillValue = -32767s ;
+                #~ v10:missing_value = -32767s ;
+                #~ v10:units = "m s**-1" ;
+                #~ v10:long_name = "10 metre V wind component" ;
 
-# tp, total precipitation
-HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Tpre
-# - daily total precipitation, NOTE: using daymax (as hourly source data are accumulative on each day)
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_total-preci_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,m.day-1      -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,tp  -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+#~ // global attributes:
+                #~ :Conventions = "CF-1.6" ;
+                #~ :history = "2020-10-16 17:30:28 GMT by grib_to_netcdf-2.16.0: /opt/ecmwf/eccodes/bin/grib_to_netcdf -S param -o /cache/data3/adaptor.mars.internal-1602868794.1346717-30509-16-627d7f99-c4cf-4e0d-bbe7-c2711994de05.nc /cache/tmp/627d7f99-c4cf-4e0d-bbe7-c2711994de05-adaptor.mars.internal-1602868794.1353724-30509-6-tmp.grib" ;
+#~ }
+#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$
 
-# ssr, surface solar radiation
-HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Snsr
-# - daily total, NOTE: using daymax (as hourly source data are accumulative on each day)
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_total-ssrad_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,J.m-2.day-1  -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,ssr -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
-# d2m
-HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Temp
-# - maximum
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-maximum_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
-# - mean
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-average_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
-# - minimum
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-minimum_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymin  -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+# steps:
+# - mergetime hourly u 
+# - mergetime hourly v
+# - cat hourly u and v 
+# - calculate hourly wind speed
+# - calculate daily value
 
-# t2m
-HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Temp
-# - maximum
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-maximum_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
-# - mean
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-average_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
-# - minimum
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-minimum_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymin  -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
-
-# fal
-HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Falb
-# - mean
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_fal-average_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,dimensionless -daymean -selyear,${YEAR} -shifttime,-25min -selvar,fal -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
-
-# sp
-HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Spre
-# - mean
-DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_spressu-avg_${YEAR}.nc
-cdo -L -b F64 -settime,00:00:00 -setunit,Pa            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,sp  -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+# calculate hourly wind speed (with expression)
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Wind
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_wind10m-avg_${YEAR}.nc
+cdo -L -setunit,"m.s-1" -daymean -expr,'wind_speed_10m = sqrt(u10*u10 + v10*v10)' -selyear,${YEAR} -shifttime,-25min -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
 
 wait
 
