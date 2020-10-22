1b0fe99455a8626792635d459821083a096e95a9
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-10-22 21:59:26 +0200

    demo to get daily values for Africa box 1

---
+++ get_daily_values/africa_example/africa_box_1/get_daily_values_all.sh
@@ -0,0 +1,97 @@
+
+set -x
+
+printf "$0"
+for arg in "$@"
+do 
+    printf " $arg"
+done
+echo
+
+echo "Arg 0: $0"
+echo "Arg 1: $1"
+
+MAIN_HOURLY_SOURCE_DIR="/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly/"
+MAIN_HOURLY_SOURCE_DIR=$1
+
+OUTPUT_FOLDER="/scratch/depfg/sutan101/test_arise_meteo_europe/daily_before_remapcon/"
+OUTPUT_FOLDER=$2
+
+YEAR=2000
+YEAR=$3
+
+YEAR_PLUS_1=$(( YEAR + 1 ))
+
+
+mkdir -p ${OUTPUT_FOLDER}
+cd ${OUTPUT_FOLDER}
+
+
+#~ (pcrglobwb_python3) sutan101@gpu040.cluster:/scratch/depfg/hoch0001/ARISE/ERA5_Land/_data/hourly$ ls -lah
+#~ total 4.0K
+#~ drwxr-xr-x 8 hoch0001 depfg   6 Oct 14 22:30 .
+#~ drwxr-xr-x 3 hoch0001 depfg   1 Oct 14 11:14 ..
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:10 Variable_Falb
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:03 Variable_Snsr
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:09 Variable_Spre
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 19 03:09 Variable_Temp
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 18 21:12 Variable_Tpre
+#~ drwxr-xr-x 2 hoch0001 depfg 468 Oct 19 03:11 Variable_Wind
+
+# tp, total precipitation
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Tpre
+# - daily total precipitation, NOTE: using daymax (as hourly source data are accumulative on each day)
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_total-preci_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,m.day-1      -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,tp  -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+
+# ssr, surface solar radiation
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Snsr
+# - daily total, NOTE: using daymax (as hourly source data are accumulative on each day)
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_total-ssrad_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,J.m-2.day-1  -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,ssr -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+
+# d2m
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Temp
+# - maximum
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-maximum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+# - mean
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-average_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+# - minimum
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_d2m-minimum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymin  -selyear,${YEAR} -shifttime,-25min -selvar,d2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+
+# t2m
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Temp
+# - maximum
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-maximum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymax  -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+# - mean
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-average_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+# - minimum
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_t2m-minimum_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,K            -daymin  -selyear,${YEAR} -shifttime,-25min -selvar,t2m -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+
+# fal
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Falb
+# - mean
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_fal-average_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,dimensionless -daymean -selyear,${YEAR} -shifttime,-25min -selvar,fal -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+
+# sp
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Spre
+# - mean
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_spressu-avg_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setunit,Pa            -daymean -selyear,${YEAR} -shifttime,-25min -selvar,sp  -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+
+# calculate hourly wind speed
+HOURLY_SOURCE_DIR=${MAIN_HOURLY_SOURCE_DIR}/Variable_Wind
+# - mean and involving expression: 'wind_speed_10m = sqrt(u10*u10 + v10*v10)'
+DAILY_OUTPUT_FILE=${OUTPUT_FOLDER}/europe_era5-land_daily_wind10m-avg_${YEAR}.nc
+cdo -L -b F64 -settime,00:00:00 -setname,"wind_speed_10m" -setunit,"m.s-1" -daymean -expr,'wind_speed_10m = sqrt(u10*u10 + v10*v10)' -selyear,${YEAR} -shifttime,-25min -mergetime ${HOURLY_SOURCE_DIR}/*${YEAR}*.nc ${HOURLY_SOURCE_DIR}/*${YEAR_PLUS_1}01.nc ${DAILY_OUTPUT_FILE} &
+
+wait
+
+set +x
