881fb6878a77aa8cab3aabc07e282a035a438531
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-10-23 00:22:42 +0200

    example to get daily values for all africa

--- get_daily_values/africa_example/africa_box_1/example_get_daily_values_all.sh
+++
@@ -1,7 +0,0 @@
-
-MAIN_HOURLY_SOURCE_DIR="/rds/general/user/esutanud/projects/arise/live/Africa/Africa-box1/"
-OUTPUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_example/Africa-box1/daily_before_remapcon/"
-
-bash get_daily_values_all.sh ${MAIN_HOURLY_SOURCE_DIR} ${OUTPUT_FOLDER} 1981
-bash get_daily_values_all.sh ${MAIN_HOURLY_SOURCE_DIR} ${OUTPUT_FOLDER} 1982 
-
