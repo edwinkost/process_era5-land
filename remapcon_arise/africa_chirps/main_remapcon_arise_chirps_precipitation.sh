cb62ec6aaa3d6f1dc5fb32e4b01113b945f7f685
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-02-23 15:25:50 +0100

    remapcon chirps precip

---
+++ remapcon_arise/africa_chirps/main_remapcon_arise_chirps_precipitation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x
+
+# chirps precipitation
+
+#~ sutan101@gpu040.cluster:/scratch/depfg/sutan101/data/chirps$ ls -lah
+#~ total 824G
+#~ drwxr-xr-x  2 sutan101 depfg    41 Feb 23 13:54 .
+#~ drwxr-xr-x 30 sutan101 depfg    29 Feb 23 07:37 ..
+#~ -rw-r-----  1 sutan101 depfg  782G Feb 23 13:34 chirps-v2.0.1981-2020.days_p05.nc
+
+INP_FOLDER="/scratch-shared/edwinhs/meteo_arise/tanzania/source_icl_downloaded_2020-03-02/"
+
+MAIN_OUT_FOLDER="/scratch/depfg/sutan101/meteo_arise/version_2021-02-XX/chirps_africa/"
+mkdir -p ${MAIN_OUT_FOLDER}
+
+# - 30 arcmin resolution
+RESOLUTION=0.5
+OUT_FOLDER=${MAIN_OUT_FOLDER}/half_arcdeg/
+bash remapcon_arise.sh ${INP_FOLDER}/chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} africa_chirps-v2.0_1981-2020_p05_rempacon-30-arcmin monsum mm.month-1
+
+# - 5 arcmin resolution
+RESOLUTION=0.083333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+OUT_FOLDER=${MAIN_OUT_FOLDER}/5_arcmin/
+bash remapcon_arise.sh ${INP_FOLDER}/chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} africa_chirps-v2.0_1981-2020_p05_rempacon-5-arcmin monsum mm.month-1
+
+# - 150 arcsec resolution
+RESOLUTION=0.041666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666667
+OUT_FOLDER=${MAIN_OUT_FOLDER}/150_arcsec/
+bash remapcon_arise.sh ${INP_FOLDER}/chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} africa_chirps-v2.0_1981-2020_p05_rempacon-150-arcsec monsum mm.month-1
+
+set +x
---
+++ remapcon_arise/africa_chirps/remapcon_arise.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+printf "$0"
+for arg in "$@"
+do 
+    printf " $arg"
+done
+echo
+
+echo "Arg 0: $0"
+echo "Arg 1: $1"
+echo "Arg 2: $2"
+echo "Arg 3: $3"
+echo "Arg 4: $4"
+echo "Arg 5: $5"
+echo "Arg 6: $6"
+
+
+# load software
+. /quanta1/home/sutan101/load_my_miniconda_and_my_default_env.sh
+
+
+set -x 
+
+INPUT_FILE="/scratch-shared/edwinhs/meteo_arise/tanzania/source_icl_downloaded_2020-03-02/chirps_rainfall/TNZ_chirps-v2.0.1981-2019.days_p05.nc"
+INPUT_FILE=$1
+
+RESOLUTION=0.041666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666667
+RESOLUTION=$2
+
+OUTPUT_DIR="/scratch-shared/edwinhs/meteo_arise/tanzania/version_2020-03-06/chirps/150_arcsec/"
+OUTPUT_DIR=$3
+
+OUTPUTFILE="tanzania_chirps-v2.0_1981-2019_p05_rempacon-150-arcsec"
+OUTPUTFILE=$4
+OUTPUT_DAILY_FILE=${OUTPUTFILE}_daily.nc
+OUTPUT_MONTH_FILE=${OUTPUTFILE}_monthly.nc
+
+CDOMON_OPERA="monsum"
+CDOMON_OPERA=$5
+MONTHLY_UNIT="mm.month-1"
+MONTHLY_UNIT=$6
+
+CLONE30SEC="/scratch-shared/edwinswt/data/pcrglobwb2_input_arise/tanzania_version_2019_12_11/pcrglobwb2_input/tanzania_30sec/cloneMaps/ghana_tanzania_uganda_zimbabwe/clone_tanzania_194_basin.map"
+CLONE30SEC="/scratch/depfg/sutan101/data/pcrglobwb_input_arise/develop/africa_30sec/cloneMaps/version_2020-XX-XX/africa_clone_30sec.map"
+
+#~ /scratch/depfg/sutan101/data/pcrglobwb_input_arise/develop/africa_30sec/cloneMaps/version_2020-XX-XX
+#~ sutan101@gpu040.cluster:/scratch/depfg/sutan101/data/pcrglobwb_input_arise/develop/africa_30sec/cloneMaps/version_2020-XX-XX$ ls -lah
+#~ total 629M
+#~ drwxr-xr-x 2 sutan101 depfg   18 Feb  5 06:55 .
+#~ drwxr-xr-x 3 sutan101 depfg    2 Feb  9 11:42 ..
+#~ -rw-r--r-- 1 sutan101 depfg  83M Oct 31  2019 africa_clone_30sec.map
+
+
+mkdir -p ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
+rm *.nc
+rm *.txt
+rm *.tif
+
+# creating griddes file
+gdalwarp -tr ${RESOLUTION} ${RESOLUTION} ${CLONE30SEC} clone.tif
+gdal_translate -of NETCDF clone.tif clone.nc
+cdo -L -griddes -invertlat clone.nc > griddes_invertlat_clone.txt
+
+# remapcon
+cdo -L -setrtoc,-inf,0,0 -settime,00:00:00 -remapcon,griddes_invertlat_clone.txt -setrtoc,-inf,0,0 ${INPUT_FILE} ${OUTPUT_DAILY_FILE}
+
+# calculate monthly values
+CDO_TIMESTAT_DATE='last' cdo -L -settime,00:00:00 -setunit,${MONTHLY_UNIT} -${CDOMON_OPERA} ${OUTPUT_DAILY_FILE} ${OUTPUT_MONTH_FILE}
+
+set +x
