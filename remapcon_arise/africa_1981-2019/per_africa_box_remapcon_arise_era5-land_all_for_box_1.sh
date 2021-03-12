8ffcfa0a8b3459678a38dd39a05fe598b3c0c9d7
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-12 15:10:19 +0100

    remapcon box1

--- remapcon_arise/africa_1981-2019/per_africa_box_remapcon_arise_era5-land_all_for_box_1.sh
+++ remapcon_arise/africa_1981-2019/per_africa_box_remapcon_arise_era5-land_all_for_box_1.sh
@@ -13,13 +13,10 @@
 
 set -x
 
-cd $HOME
-
 # load the required modules
+cd $HOME
 . load_all_default.sh
 
-
-OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa-box1/daily_after_remapcon/merged_1981-2019/"
 
 INP_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa-box1/daily_before_remapcon/merged_1981-2019/"
 
@@ -36,8 +33,12 @@ INP_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/af
 #~ -rw------- 1 esutanud hpc-ec407 48G Mar  4 08:20 era5-land_daily_total-ssrad_1981-2019.nc
 #~ -rw------- 1 esutanud hpc-ec407 48G Mar  4 08:20 era5-land_daily_wind10m-avg_1981-2019.nc
 
+
+OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa-box1/daily_after_remapcon/merged_1981-2019/"
+
 # making output folder 
 mkdir -p ${OUT_FOLDER}
+
 
 # making the clone map at 30sec
 CLONE30SEC_FILE=${OUT_FOLDER}/clone_africa_30sec.map
@@ -48,6 +49,13 @@ CLONE30SEC_FILE=${OUT_FOLDER}/clone_africa_30sec.map
 #~ nrows for 30sec	= 9000
 mapattr -s -R 9000 -C 8520 -B -P yb2t -x -19 -y 39 -l 0.008333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333 ${CLONE30SEC_FILE}
 
+
+# go to the folder that contains this script
+cd $HOME
+cd github/edwinkost/process_era5-land/remapcon_arise/africa_1981-2019
+
+
+# run the tasks
 
 bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_total-preci_1981-1989.nc ${OUT_FOLDER} era5-land_daily_total-preci monsum  "m.month-1"   ${CLONE30SEC_FILE} &
 
