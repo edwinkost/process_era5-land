89eeea4c4bc39eb8cdfde34aa8ea08f888cf3c22
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-12 15:25:17 +0100

    remapcon box1

--- remapcon_arise/africa_1981-2019/per_africa_box_remapcon_arise_era5-land_all_for_box_1.sh
+++ remapcon_arise/africa_1981-2019/per_africa_box_remapcon_arise_era5-land_all_for_box_1.sh
@@ -57,23 +57,23 @@ cd github/edwinkost/process_era5-land/remapcon_arise/africa_1981-2019
 
 # run the tasks
 
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_total-preci_1981-1989.nc ${OUT_FOLDER} era5-land_daily_total-preci monsum  "m.month-1"   ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_total-preci_1981-2019.nc ${OUT_FOLDER} era5-land_daily_total-preci monsum  "m.month-1"   ${CLONE30SEC_FILE} &
 
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_d2m-average_1981-1989.nc ${OUT_FOLDER} era5-land_daily_d2m-average monmean "K"           ${CLONE30SEC_FILE} &
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_d2m-maximum_1981-1989.nc ${OUT_FOLDER} era5-land_daily_d2m-maximum monmean "K"           ${CLONE30SEC_FILE} &
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_d2m-minimum_1981-1989.nc ${OUT_FOLDER} era5-land_daily_d2m-minimum monmean "K"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_d2m-average_1981-2019.nc ${OUT_FOLDER} era5-land_daily_d2m-average monmean "K"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_d2m-maximum_1981-2019.nc ${OUT_FOLDER} era5-land_daily_d2m-maximum monmean "K"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_d2m-minimum_1981-2019.nc ${OUT_FOLDER} era5-land_daily_d2m-minimum monmean "K"           ${CLONE30SEC_FILE} &
 
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_t2m-average_1981-1989.nc ${OUT_FOLDER} era5-land_daily_t2m-average monmean "K"           ${CLONE30SEC_FILE} &
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_t2m-maximum_1981-1989.nc ${OUT_FOLDER} era5-land_daily_t2m-maximum monmean "K"           ${CLONE30SEC_FILE} &
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_t2m-minimum_1981-1989.nc ${OUT_FOLDER} era5-land_daily_t2m-minimum monmean "K"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_t2m-average_1981-2019.nc ${OUT_FOLDER} era5-land_daily_t2m-average monmean "K"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_t2m-maximum_1981-2019.nc ${OUT_FOLDER} era5-land_daily_t2m-maximum monmean "K"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_t2m-minimum_1981-2019.nc ${OUT_FOLDER} era5-land_daily_t2m-minimum monmean "K"           ${CLONE30SEC_FILE} &
 
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_spressu-avg_1981-1989.nc ${OUT_FOLDER} era5-land_daily_spressu-avg monmean "1"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_spressu-avg_1981-2019.nc ${OUT_FOLDER} era5-land_daily_spressu-avg monmean "1"           ${CLONE30SEC_FILE} &
 
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_fal-average_1981-1989.nc ${OUT_FOLDER} era5-land_daily_fal-average monmean "1"           ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_fal-average_1981-2019.nc ${OUT_FOLDER} era5-land_daily_fal-average monmean "1"           ${CLONE30SEC_FILE} &
 
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_total-ssrad_1981-1989.nc ${OUT_FOLDER} era5-land_daily_total-ssrad monmean "J.m-2.day-1" ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_total-ssrad_1981-2019.nc ${OUT_FOLDER} era5-land_daily_total-ssrad monmean "J.m-2.day-1" ${CLONE30SEC_FILE} &
 
-bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_wind10m-avg_1981-1989.nc ${OUT_FOLDER} era5-land_daily_wind10m-avg monmean "m.s-1"       ${CLONE30SEC_FILE} &
+bash remapcon_arise_era5.sh ${INP_FOLDER} era5-land_daily_wind10m-avg_1981-2019.nc ${OUT_FOLDER} era5-land_daily_wind10m-avg monmean "m.s-1"       ${CLONE30SEC_FILE} &
 
 wait
 
