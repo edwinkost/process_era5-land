2219a8dd30559d9b76de09ff9f859e3d69442ac8
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-16 20:35:11 +0100

    merging grids africa 1981-2019

---
+++ unify_mergegrid/main_merge_grid_africa_1981-2019.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#~ #PBS -l walltime=24:00:00
+#~ #PBS -l select=1:ncpus=8:mem=64gb
+
+#PBS -q express -P exp-00044
+
+#~ #PBS -l walltime=48:00:00
+#~ #PBS -l select=1:ncpus=16:mem=62gb
+
+#PBS -l walltime=70:00:00
+#PBS -l select=1:ncpus=48:mem=126gb
+
+#PBS -N mergegrid_1981-2019
+
+set -x
+
+# load the required modules
+. /rds/general/user/esutanud/home/load_all_default.sh
+
+# go to the folder where the scripts are stored
+cd /rds/general/user/esutanud/home/github/edwinkost/process_era5-land/unify_mergegrid/
+
+#~ OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_example/africa_merged/1981-1989/"
+#~ INP_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_example/"
+
+INP_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/"
+
+OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa_merged/1981-2019/"
+
+
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-average &
+
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_fal-average &
+
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_spressu-avg &
+
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-average &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-maximum &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-minimum &
+
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_total-ssrad &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_wind10m-avg &
+
+wait
+
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-maximum &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-minimum &
+
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_total-preci &
+
+wait
+
+set +x
--- unify_mergegrid/merge_grid_africa.sh
+++ unify_mergegrid/merge_grid_africa.sh
@@ -13,18 +13,40 @@ echo "Arg 3: $3"
 
 set -x
 
-#~ esutanud@login-7:/rds/general/user/esutanud/ephemeral/meteo_arise/africa_example$ ls -lah Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_*/*daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_d2m-average/era5-land_daily_d2m-average_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_d2m-maximum/era5-land_daily_d2m-maximum_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_d2m-minimum/era5-land_daily_d2m-minimum_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_fal-average/era5-land_daily_fal-average_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_spressu-avg/era5-land_daily_spressu-avg_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_t2m-average/era5-land_daily_t2m-average_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_t2m-maximum/era5-land_daily_t2m-maximum_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_t2m-minimum/era5-land_daily_t2m-minimum_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_total-preci/era5-land_daily_total-preci_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_total-ssrad/era5-land_daily_total-ssrad_rempacon-150-arcsec_daily.nc
-#~ -rw------- 1 esutanud hpc-ec407 84G Nov 25 21:18 Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/era5-land_daily_wind10m-avg/era5-land_daily_wind10m-avg_rempacon-150-arcsec_daily.nc
+#~ (pcrglobwb_python3) esutanud@login-5:/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019$ ls -lah africa-box*/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_*/*daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:18 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-average/era5-land_daily_d2m-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:16 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-maximum/era5-land_daily_d2m-maximum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:18 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-minimum/era5-land_daily_d2m-minimum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:16 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_fal-average/era5-land_daily_fal-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:25 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_spressu-avg/era5-land_daily_spressu-avg_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:17 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-average/era5-land_daily_t2m-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:18 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-maximum/era5-land_daily_t2m-maximum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:24 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-minimum/era5-land_daily_t2m-minimum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:14 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_total-preci/era5-land_daily_total-preci_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:18 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_total-ssrad/era5-land_daily_total-ssrad_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:21 africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_wind10m-avg/era5-land_daily_wind10m-avg_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:00 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-average/era5-land_daily_d2m-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:04 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-maximum/era5-land_daily_d2m-maximum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 13:58 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-minimum/era5-land_daily_d2m-minimum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 323G Mar 16 13:57 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_fal-average/era5-land_daily_fal-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:01 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_spressu-avg/era5-land_daily_spressu-avg_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:03 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-average/era5-land_daily_t2m-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:07 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-maximum/era5-land_daily_t2m-maximum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:02 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-minimum/era5-land_daily_t2m-minimum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:09 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_total-preci/era5-land_daily_total-preci_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:02 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_total-ssrad/era5-land_daily_total-ssrad_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 326G Mar 16 14:04 africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_wind10m-avg/era5-land_daily_wind10m-avg_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 279G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-average/era5-land_daily_d2m-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 279G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-maximum/era5-land_daily_d2m-maximum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 278G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_d2m-minimum/era5-land_daily_d2m-minimum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 276G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_fal-average/era5-land_daily_fal-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 276G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_spressu-avg/era5-land_daily_spressu-avg_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 278G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-average/era5-land_daily_t2m-average_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 278G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-maximum/era5-land_daily_t2m-maximum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 275G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_t2m-minimum/era5-land_daily_t2m-minimum_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 279G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_total-preci/era5-land_daily_total-preci_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 276G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_total-ssrad/era5-land_daily_total-ssrad_rempacon-150-arcsec_daily.nc
+#~ -r-------- 1 esutanud hpc-ec407 279G Mar 16 13:15 africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/era5-land_daily_wind10m-avg/era5-land_daily_wind10m-avg_rempacon-150-arcsec_daily.nc
 
 
 MAIN_INPUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_example/"
@@ -47,29 +69,29 @@ INP_FILENAME=$3/$3_rempacon-150-arcsec_daily.nc
 OUT_FILENAME=${DAILY_OUTPUT_FOLDER}/"africa_era5-land_daily_total-preci_rempacon-150-arcsec_daily.nc"
 OUT_FILENAME=${DAILY_OUTPUT_FOLDER}/africa_$3_rempacon-150-arcsec_daily.nc
 
-BOX1_FILENAME=${MAIN_INPUT_FOLDER}"/Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/${INP_FILENAME}"
-BOX2_FILENAME=${MAIN_INPUT_FOLDER}"/Africa-box2/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/${INP_FILENAME}"
-BOX3_FILENAME=${MAIN_INPUT_FOLDER}"/Africa-box3/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/${INP_FILENAME}"
+BOX1_FILENAME=${MAIN_INPUT_FOLDER}"/africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/${INP_FILENAME}"
+BOX2_FILENAME=${MAIN_INPUT_FOLDER}"/africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/${INP_FILENAME}"
+BOX3_FILENAME=${MAIN_INPUT_FOLDER}"/africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/${INP_FILENAME}"
 
-cdo -L -mergegrid ${BOX3_FILENAME} -mergegrid ${BOX1_FILENAME} ${BOX2_FILENAME} ${OUT_FILENAME} &
+cdo -L -f nc4 -mergegrid ${BOX3_FILENAME} -mergegrid ${BOX1_FILENAME} ${BOX2_FILENAME} ${OUT_FILENAME} &
 
 
-#~ # merging monthly files 
+# merging monthly files 
 
-#~ MONTHLY_OUTPUT_FOLDER=${OUTPUT_FOLDER}/monthly_but_merged_from_boxes/
-#~ mkdir -p ${MONTHLY_OUTPUT_FOLDER}
+MONTHLY_OUTPUT_FOLDER=${OUTPUT_FOLDER}/monthly_but_merged_from_boxes/
+mkdir -p ${MONTHLY_OUTPUT_FOLDER}
 
-#~ INP_FILENAME="era5-land_daily_total-preci/era5-land_daily_total-preci_rempacon-150-arcsec_monthly.nc"
-#~ INP_FILENAME=$3/$3_rempacon-150-arcsec_monthly.nc
+INP_FILENAME="era5-land_daily_total-preci/era5-land_daily_total-preci_rempacon-150-arcsec_monthly.nc"
+INP_FILENAME=$3/$3_rempacon-150-arcsec_monthly.nc
 
-#~ OUT_FILENAME=${MONTHLY_OUTPUT_FOLDER}/"africa_era5-land_daily_total-preci_rempacon-150-arcsec_monthly.nc"
-#~ OUT_FILENAME=${MONTHLY_OUTPUT_FOLDER}/africa_$3_rempacon-150-arcsec_monthly.nc
+OUT_FILENAME=${MONTHLY_OUTPUT_FOLDER}/"africa_era5-land_daily_total-preci_rempacon-150-arcsec_monthly.nc"
+OUT_FILENAME=${MONTHLY_OUTPUT_FOLDER}/africa_$3_rempacon-150-arcsec_monthly.nc
 
-#~ BOX1_FILENAME=${MAIN_INPUT_FOLDER}"/Africa-box1/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/${INP_FILENAME}"
-#~ BOX2_FILENAME=${MAIN_INPUT_FOLDER}"/Africa-box2/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/${INP_FILENAME}"
-#~ BOX3_FILENAME=${MAIN_INPUT_FOLDER}"/Africa-box3/daily_after_remapcon/merged_1981-1989_with_jobs/150_arcsec/${INP_FILENAME}"
+BOX1_FILENAME=${MAIN_INPUT_FOLDER}"/africa-box1/daily_after_remapcon/merged_1981-2019/150_arcsec/${INP_FILENAME}"
+BOX2_FILENAME=${MAIN_INPUT_FOLDER}"/africa-box2/daily_after_remapcon/merged_1981-2019/150_arcsec/${INP_FILENAME}"
+BOX3_FILENAME=${MAIN_INPUT_FOLDER}"/africa-box3/daily_after_remapcon/merged_1981-2019/150_arcsec/${INP_FILENAME}"
 
-#~ cdo -L -mergegrid ${BOX3_FILENAME} -mergegrid ${BOX1_FILENAME} ${BOX2_FILENAME} ${OUT_FILENAME} &
+cdo -L -f nc4 -mergegrid ${BOX3_FILENAME} -mergegrid ${BOX1_FILENAME} ${BOX2_FILENAME} ${OUT_FILENAME} &
 
 wait
 
