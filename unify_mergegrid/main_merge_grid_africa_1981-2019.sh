300b254caec98328598e4551b9b4d16b41a9e9da
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-18 10:50:52 +0100

    merging grids for africa

--- unify_mergegrid/main_merge_grid_africa_1981-2019.sh
+++ unify_mergegrid/main_merge_grid_africa_1981-2019.sh
@@ -29,31 +29,31 @@ cd /rds/general/user/esutanud/home/github/edwinkost/process_era5-land/unify_merg
 
 INP_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/"
 
-#~ OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa_merged/1981-2019/"
+OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa_merged/1981-2019/"
 
-OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa_merged/1981-2019_test_t2m_only/"
+#~ OUT_FOLDER="/rds/general/user/esutanud/ephemeral/meteo_arise/africa_1981-2019/africa_merged/1981-2019_test_t2m_only/"
 
 
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-average &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-average &
 
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_fal-average &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_fal-average &
 
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_spressu-avg &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_spressu-avg &
 
 bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-average &
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-maximum &
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-minimum &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-maximum &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_t2m-minimum &
 
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_total-ssrad &
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_wind10m-avg &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_total-ssrad &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_wind10m-avg &
 
 wait
 
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-maximum &
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-minimum &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-maximum &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_d2m-minimum &
 
-#~ bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_total-preci &
+bash merge_grid_africa.sh ${INP_FOLDER} ${OUT_FOLDER} era5-land_daily_total-preci &
 
-#~ wait
+wait
 
 set +x
